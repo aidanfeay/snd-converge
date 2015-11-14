84cae45cca4a30c75ebe9095320f6aaeec77ee13
Ashlyn Still <ashlyn.still@gmail.com>
2015-11-14 11:34:36 -0600

    set up basic modal overlay and article text

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+var converge = document.getElementById('converge-btn'),
+	modal = document.getElementById('converge-modal-overlay');
+
+
+console.log(converge);
+
+converge.addEventListener('click', function(){
+
+	modal.style.display = 'block'
+});
