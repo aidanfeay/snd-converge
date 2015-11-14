c3a97ae6591cbd74c16661ac9fc253a7e7160f0b
Josh Kadis <josh@alleyinteractive.com>
2015-11-14 17:53:52 -0600

    more event routing

--- assets/scripts/dashboard/router.js
+++ assets/scripts/dashboard/router.js
@@ -6,15 +6,23 @@ define( ['jquery','underscore', 'backbone', 'views/event'] , function($, _, Back
 	var Workspace = Backbone.Router.extend({
 
 		routes : {
-			"platform/event/:event" : "event"
+			"platform/event/:event" : "event",
+			"platform/event(/)" : "randomizeEvent"
 		},
 
 		initialize : function(){
 			Backbone.history.start({pushState: true});
 		},
 
+		// show event based on route to slug
 		event: function(route) {
 			new EventView(route);
+		},
+
+		// URL platform/event returns a random event from the collection
+		randomizeEvent: function() {
+			var eventSlugs = Converge.events.pluck('slug');
+			this.event(_.sample(eventSlugs, 1)[0]);
 		}
 
 	});
