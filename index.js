675abc846a671f0e81c1dd17b6097197aaa603fb
Aidan Feay <aidanlfeay@gmail.com>
2015-11-14 08:41:16 -0600

    pages and partials init

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Index is running...');
+app.get('/', function(request, response) {
+  response.render('pages/home');
 });
+
+app.use(express.static(__dirname + '/assets'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 var server = app.listen(process.env.PORT || 5000, function () {
   var host = server.address().address;
