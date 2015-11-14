6546cd63a574c3e977bd1cc43efb37ba3c73dd88
Ashlyn Still <ashlyn.still@gmail.com>
2015-11-14 14:53:30 -0600

    add bottom row buttons/styles

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,10 @@
 $converge = $('#converge-btn'),
-$modal = $('#converge-modal-overlay');
+$modal = $('#converge-modal-overlay'),
+$close = $('#close-modal');
 
 $converge.on('click', function(){
 	$modal.fadeIn('fast');
+});
+$close.on('click', function(){
+	$modal.fadeOut('fast');
 });
